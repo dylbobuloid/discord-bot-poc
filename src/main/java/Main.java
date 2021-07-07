6ff4c249c44327c8a7e05ef8d9c9f0416bf15295
Dylan Fernandes <majestickookie@gmail.com>
2021-07-07 18:40:38 +0100

    Implemented hanz command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
 import org.jetbrains.annotations.NotNull;
 
@@ -15,13 +19,31 @@ public class Main implements EventListener {
 
         JDA jda = JDABuilder.createDefault(args[0])             //Creating instance of JDA
                 .addEventListeners(new Main()).build();
+
+
     }
+
 
     @Override
     public void onEvent(GenericEvent event){
         if(event instanceof ReadyEvent)
-            System.out.println("API is ready");
+            System.out.println("API is ready");                 // Checks if the bot is available
     }
+
+    @Override
+    public void onMessageRecieved(MessageReceivedEvent event){
+        if (msg.getContentRaw().equals("!hanz")){
+
+            event.getChannel().sendMessage("https://tenor.com/view/hasbulla-hasbullah-gif-21708537").queue();
+        }
+    }
+
+/*    public void setActivity(Activity activity){
+
+    }*/
+
+/*    public void setStatus(OnlineStatus status){                 // Setting the status of the bot
+    }*/
 
 
 }
